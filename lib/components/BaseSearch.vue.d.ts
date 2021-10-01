f27ff6cf2f86069f0fa97ec1d73e53de682036aa
Scazi <scazi@inuits.eu>
2021-10-01 17:06:11 +0200

    Improved responsiveness search bar

--- lib/components/BaseSearch.vue.d.ts
+++ lib/components/BaseSearch.vue.d.ts
@@ -2,6 +2,9 @@ declare const _default: import("vue").DefineComponent<{
     label: {
         type: StringConstructor;
     };
+    searchLabel: {
+        type: StringConstructor;
+    };
     modelValue: {
         type: StringConstructor;
         default: string;
@@ -19,6 +22,7 @@ declare const _default: import("vue").DefineComponent<{
     onClick: () => void;
 }, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, ("update:modelValue" | "onClick")[], "update:modelValue" | "onClick", import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
     label?: unknown;
+    searchLabel?: unknown;
     modelValue?: unknown;
     debounce?: unknown;
     debounceWait?: unknown;
@@ -28,6 +32,7 @@ declare const _default: import("vue").DefineComponent<{
     debounceWait: number;
 } & {
     label?: string | undefined;
+    searchLabel?: string | undefined;
 }> & {
     "onUpdate:modelValue"?: ((...args: any[]) => any) | undefined;
     onOnClick?: ((...args: any[]) => any) | undefined;
--- src/components/BaseSearch.vue
+++ src/components/BaseSearch.vue
@@ -1,9 +1,9 @@
 <template>
   <label class="flex block my-2 text-center text-text-dark py-2 shadow-lg pl-2 pr-8 w-full min-w-48 text-neutral-700 text-xl focus:outline-none rounded-full bg-text-white">
-    <span v-if="label" class="ml-1 px-4 flex items-center font-bold text-sm rounded-full bg-accent-purple text-text-white">{{ label }}</span>
+    <span v-if="label" class="ml-1 px-4 items-center font-bold text-sm rounded-full bg-accent-purple text-text-white hidden sm:flex">{{ label }}</span>
     <input v-model="inputValue" v-bind="$attrs" type="text" class="w-full text-center" />
     <a @click="onClick" class="flex items-center pt-2 pb-2 cursor-pointer hover:text-accent-purple">
-      <svg class="mr-2 stroke-current" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg class="mr-2 stroke-current float-right sm:float-none" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M7 13C10.3137 13 13 10.3137 13 7C13 3.68629 10.3137 1 7 1C3.68629 1 1 3.68629 1 7C1 10.3137 3.68629 13 7 13Z"
           stroke="#2D242A"
@@ -14,7 +14,7 @@
         <path d="M15.0002 15L11.2422 11.242" stroke="#2D242A" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
       </svg>
 
-      Zoeken
+     <p class="hidden sm:flex"> {{searchLabel}}</p>
     </a>
   </label>
 </template>
@@ -28,6 +28,7 @@ export default defineComponent({
   inheritAttrs: false,
   props: {
     label: { type: String },
+    searchLabel: {type: String},
     modelValue: { type: String, default: '' },
     debounce: { type: Boolean, default: false },
     debounceWait: { type: Number, default: 400 },
