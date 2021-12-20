ce2b303a2aca24a9c912c7f02161bac815f690c9
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-12-20 16:33:18 +0100

    Add word break to basemetadata

--- src/components/BaseMetaData.vue
+++ src/components/BaseMetaData.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="grid grid-flow-col-dense auto-cols-fr">
-    <strong class="col-start-1 w-min inline-block">{{ keyWord }}</strong
+    <strong
+      class="col-start-1 w-full inline-block overflow-hidden break-words"
+      >{{ keyWord }}</strong
     ><span v-if="type != ''" class="col-start-2 mr-10">{{ type }} </span>
-    <span v-if="type == ''" class="text-text-red col-start-2 mr-10">{{ errorText }}</span>
+    <span v-if="type == ''" class="text-text-red col-start-2 mr-10">{{
+      errorText
+    }}</span>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue"
 
 export default defineComponent({
   props: {
