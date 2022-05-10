3e9a53a2d112d8e8646517dfa64f47c35687319d
Berend <berend@inuits.eu>
2022-05-10 16:29:03 +0200

    fix merge conflict

--- src/components/TheCarousel.vue
+++ src/components/TheCarousel.vue
@@ -32,11 +32,28 @@
         />
       </a>
 
-      <IIIFViewer v-if="mediafiles[selectedIndex].mediatype.image" :canGoFullScreen="isTouch ? false : true" :imageUrl="source[selectedIndex].infoJson" />
+      <IIIFViewer
+        v-if="mediafiles[selectedIndex].mediatype.image"
+        :canGoFullScreen="isTouch ? false : true"
+        :imageUrl="source[selectedIndex].infoJson"
+      />
 
-      <VideoPlayer class="pb-6" v-if="mediafiles[selectedIndex].mediatype.video" :mediaFile="mediafiles[selectedIndex]" />
-      <AudioPlayer v-if="mediafiles[selectedIndex].mediatype.audio || mediafiles[selectedIndex].filename.includes('.mp3', '.wav')" :mediaFile="mediafiles[selectedIndex]" />
-      <PDFViewer v-if="mediafiles[selectedIndex].mediatype.pdf" :mediaFile="mediafiles[selectedIndex]" />
+      <VideoPlayer
+        class="pb-6"
+        v-if="mediafiles[selectedIndex].mediatype.video"
+        :mediaFile="mediafiles[selectedIndex]"
+      />
+      <AudioPlayer
+        v-if="
+          mediafiles[selectedIndex].mediatype.audio ||
+          mediafiles[selectedIndex].filename.includes('.mp3', '.wav')
+        "
+        :mediaFile="mediafiles[selectedIndex]"
+      />
+      <PDFViewer
+        v-if="mediafiles[selectedIndex].mediatype.pdf"
+        :mediaFile="mediafiles[selectedIndex]"
+      />
     </section>
   </base-modal>
   <!--Carousel -->
@@ -157,7 +174,6 @@
   </div>
 </template>
 <script lang="ts">
-<<<<<<< HEAD
 import {
   defineComponent,
   onMounted,
@@ -174,19 +190,8 @@ import CopyrightTab from "./CopyrightTab.vue";
 import LazyLoadImage from "./LazyLoadImage.vue";
 import { ImageSource, ModalState } from "@/types";
 import VideoPlayer from "./VideoPlayer.vue";
-=======
-import { defineComponent, onMounted, PropType, ref, SetupContext, watch } from 'vue'
-import BaseButton from './BaseButton.vue'
-import BaseModal from './BaseModal.vue'
-import BaseIcon from './BaseIcon.vue'
-import IIIFViewer from './IIIFViewer.vue'
-import CopyrightTab from './CopyrightTab.vue'
-import LazyLoadImage from './LazyLoadImage.vue'
-import { ImageSource, ModalState } from '@/types'
-import VideoPlayer from './VideoPlayer.vue'
-import AudioPlayer from './AudioPlayer.vue'
+import AudioPlayer from "./AudioPlayer.vue";
 //import PDFViewer from './PDFViewer.vue'
->>>>>>> 9bcd82f (final mime changes)
 
 export default defineComponent({
   props: {
