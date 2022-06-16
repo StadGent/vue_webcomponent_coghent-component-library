ebe2135c754ff527c36be3f1b0d9f9c7993ea426
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-06-16 16:22:55 +0200

    #95810 Create storybox from entity + get assets dynamically

--- src/composables/useStoryBox.ts
+++ src/composables/useStoryBox.ts
@@ -1,19 +1,16 @@
-import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
-import { provideApolloClient, useQuery } from "@vue/apollo-composable";
+import { MetaKey } from '@/queries';
+import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import { provideApolloClient, useQuery } from '@vue/apollo-composable';
 import { ref } from "vue";
-import {
-  CreateStoryboxDocument,
-  Entity,
-  StoryboxBuild,
-  StoryboxDocument,
-} from "..";
+import { CreateStoryboxDocument, Entity, GetEntityByIdDocument, KeyValuePair, StoryboxBuild, StoryboxDocument } from "..";
 
 export type StoryBoxType = {
   count: number;
   storyboxes: Entity[];
+  activeStoryboxAssets: Array<Entity>
 };
 
-export const StoryBoxState = ref<StoryBoxType>({ count: 0, storyboxes: [] });
+export const StoryBoxState = ref<StoryBoxType>({ count: 0, storyboxes: [], activeStoryboxAssets: [] });
 
 export const useStorybox = (_client: ApolloClient<NormalizedCacheObject>) => {
   const apolloProvider = provideApolloClient(_client);
@@ -67,6 +64,38 @@ export const useStorybox = (_client: ApolloClient<NormalizedCacheObject>) => {
     return frame;
   };
 
+  const createStoryboxFromEntity = (_entity: Entity) => {
+    console.log(`+ createStoryboxFromEntity`, _entity);
+    let storybox: StoryboxBuild = {}
+    // console.log('+ title from entity', _entity.metadata.filter(data => data?.key === MetaKey.Title))
+    // console.log('+ description from entity', _entity.metadata.filter(data => data?.key === MetaKey.Description))
+    console.log('+ relations from entity', _entity.relations)
+    storybox.frameId = _entity.id
+    storybox.assets = _entity.relations?.map(_relation => _relation?.key.replace(`entities/`, '')) as Array<string>
+    storybox.assetTimings = []
+    if (_entity.relations) {
+      for (const _relation of _entity.relations) {
+        storybox.assetTimings.push({ key: _relation?.key.replace(`entities/`, ''), value: String(_relation?.timestamp_end! - _relation?.timestamp_zoom!) } as KeyValuePair)
+      }
+    }
+    console.log(`+ storybox from entity`, storybox)
+    return storybox
+  }
+
+  const getAssets = async (_assetIds: Array<string>) => {
+    const { fetchMore } = apolloProvider(() =>
+      useQuery(GetEntityByIdDocument, { id: '' })
+    );
+    if (_assetIds.length >= 0) {
+      for (const assetId of _assetIds) {
+        const result = await fetchMore({ variables: { id: assetId } })
+        const asset = result?.data?.Entity as any
+        StoryBoxState.value.activeStoryboxAssets.push(asset as Entity)
+      }
+    }
+    return StoryBoxState.value.activeStoryboxAssets
+  }
+
   return {
     setStoryBoxes,
     addStoryBoxes,
@@ -74,5 +103,7 @@ export const useStorybox = (_client: ApolloClient<NormalizedCacheObject>) => {
     getStoryboxes,
     createNew,
     getStoryBoxById,
+    createStoryboxFromEntity,
+    getAssets,
   };
 };
