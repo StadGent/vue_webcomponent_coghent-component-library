d1ff9bcfe5e58cc07454ec10c7f99cdcddb03d43
Berend <berend@inuits.eu>
2022-06-16 13:59:13 +0200

    Added missing return

--- src/composables/useStoryBox.ts
+++ src/composables/useStoryBox.ts
@@ -1,10 +1,15 @@
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
-import { provideApolloClient, useQuery } from '@vue/apollo-composable';
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
+import { provideApolloClient, useQuery } from "@vue/apollo-composable";
 import { ref } from "vue";
-import { CreateStoryboxDocument, Entity, StoryboxBuild, StoryboxDocument } from "..";
+import {
+  CreateStoryboxDocument,
+  Entity,
+  StoryboxBuild,
+  StoryboxDocument,
+} from "..";
 
 export type StoryBoxType = {
-  count: number
+  count: number;
   storyboxes: Entity[];
 };
 
@@ -12,7 +17,6 @@ export const StoryBoxState = ref<StoryBoxType>({ count: 0, storyboxes: [] });
 
 export const useStorybox = (_client: ApolloClient<NormalizedCacheObject>) => {
   const apolloProvider = provideApolloClient(_client);
-
 
   const setStoryBoxes = (newStoryBoxes: Entity[]) => {
     StoryBoxState.value.storyboxes = newStoryBoxes;
@@ -44,25 +48,24 @@ export const useStorybox = (_client: ApolloClient<NormalizedCacheObject>) => {
   };
 
   const getStoryboxes = async () => {
-    const { fetchMore } = apolloProvider(() =>
-      useQuery(StoryboxDocument)
-    );
-    const result = await fetchMore({})
-    StoryBoxState.value.storyboxes = result?.data.Storybox?.results as Array<Entity>
-    StoryBoxState.value.count = result?.data.Storybox?.count as number
-    return StoryBoxState
-  }
+    const { fetchMore } = apolloProvider(() => useQuery(StoryboxDocument));
+    const result = await fetchMore({});
+    StoryBoxState.value.storyboxes = result?.data.Storybox
+      ?.results as Array<Entity>;
+    StoryBoxState.value.count = result?.data.Storybox?.count as number;
+    return StoryBoxState;
+  };
 
   const createNew = async (_storyboxInfo: StoryboxBuild) => {
     const { fetchMore } = apolloProvider(() =>
       useQuery(CreateStoryboxDocument, { storyboxInfo: {} })
     );
     const frame = await fetchMore({
-      variables: { storyboxInfo: _storyboxInfo }
-    })
-    await getStoryboxes()
-    return frame
-  }
+      variables: { storyboxInfo: _storyboxInfo },
+    });
+    await getStoryboxes();
+    return frame;
+  };
 
   return {
     setStoryBoxes,
@@ -70,5 +73,6 @@ export const useStorybox = (_client: ApolloClient<NormalizedCacheObject>) => {
     deleteStoryBoxes,
     getStoryboxes,
     createNew,
+    getStoryBoxById,
   };
 };
