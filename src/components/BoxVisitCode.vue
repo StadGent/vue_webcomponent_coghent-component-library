46a3b900030e50f6217414d9d7fa2eac473413f2
Berend <berend@inuits.eu>
2022-06-20 10:07:40 +0200

    Refs #96152 Moved boxvisitcode component to component library

---
+++ src/components/BoxVisitCode.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="flex w-full justify-center align-center text-3xl font-bold">
+    <div
+      v-for="(number, index) in codeNumberArray"
+      :key="index"
+      class="p-6 number-container mx-3 flex justify-center items-center rounded-xl"
+    >
+      <p>{{ number }}</p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref, watch } from 'vue';
+
+export default defineComponent({
+  name: 'BoxVisitCode',
+  components: {
+      
+  },
+  props: {
+      code: {
+        type: String,
+        required: true,
+      }
+  },
+  setup: (props) => {
+      const codeNumberArray = ref<Array<number>>([])
+      
+      const numberArray = props.code.split('').map((number) => {
+          return Number(number)
+      })
+      codeNumberArray.value = numberArray
+
+    return {
+        codeNumberArray
+    }
+
+  },
+});
+</script>
+
+<style scoped>
+.number-container{
+    background-color: #F0EDE6;
+}
+</style>
