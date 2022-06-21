c344114e731f6f2f4365709b9d0b494a460a0c8f
Berend <berend@inuits.eu>
2022-06-21 09:34:23 +0200

    CL version bump

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -49,8 +49,7 @@ type CustomIcon =
   | "copy"
   | "thumbsup"
   | "surprise"
-  | "newItem"
-  | "qrCode";
+  | "newItem";
 export default defineComponent({
   props: {
     icon: {
@@ -120,9 +119,9 @@ export default defineComponent({
     <path stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M8 15c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zM8 12V8"/>
 </svg>`,
       link: `<svg xmlns="http://www.w3.org/2000/svg" width="${
-        props.width ? props.width : 16
+        props.width ? props.width : 24
       }" height="${
-        props.height ? props.height : 16
+        props.height ? props.height : 24
       }" fill="none" viewBox="0 0 16 16">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M8.995 7.005c1.375 1.374 1.375 3.601 0 4.975l-1.99 1.99c-1.373 1.374-3.6 1.374-4.974 0-1.375-1.374-1.375-3.6 0-4.975l1.748-1.698"/>
     <path stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M7.005 8.995c-1.374-1.374-1.374-3.601 0-4.975l1.99-1.99c1.374-1.374 3.601-1.374 4.975 0 1.374 1.374 1.374 3.601 0 4.975l-1.748 1.698"/>
@@ -563,11 +562,6 @@ export default defineComponent({
 <path d="M12.5 0.5V6.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M9.5 3.5H15.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`,
-      qrCode: `<svg xmlns="http://www.w3.org/2000/svg" width="${
-        props.width ? props.width : 24
-      }" height="${
-        props.height ? props.height : 24
-      }" viewBox="0 0 24 24"><path d="M21 21v3h3v-3h-1v-1h-1v1h-1zm2 1v1h-1v-1h1zm-23 2h8v-8h-8v8zm1-7h6v6h-6v-6zm20 3v-1h1v1h-1zm-19-2h4v4h-4v-4zm8-3v2h-1v-2h1zm2-8h1v1h-1v-1zm1-1h1v1h-1v-1zm1 2v-1h1v1h-1zm0-2h1v-6h-3v1h-3v1h3v1h2v3zm-1-4v-1h1v1h-1zm-7 4h-4v-4h4v4zm6 0h-1v-2h-2v-1h3v1h1v1h-1v1zm-4-6h-8v8h8v-8zm-1 7h-6v-6h6v6zm3 0h-1v-1h2v2h-1v-1zm-3 3v1h-1v-1h1zm15 6h2v3h-1v-1h-2v1h-1v-1h-1v-1h1v-1h1v1h1v-1zm-4 2h-1v1h-1v-1h-1v-1h1v-1h-2v-1h-1v1h-1v1h-2v1h-1v6h5v-1h-1v-2h-1v2h-2v-1h1v-1h-1v-1h3v-1h2v2h-1v1h1v2h1v-2h1v1h1v-1h1v-2h1v-1h-2v-1zm-1 3h-1v-1h1v1zm6-6v-2h1v2h-1zm-9 5v1h-1v-1h1zm5 3v-1h1v2h-2v-1h1zm-3-23v8h8v-8h-8zm7 7h-6v-6h6v6zm-1-1h-4v-4h4v4zm1 4h1v2h-1v1h-2v-4h1v2h1v-1zm-4 6v-3h1v3h-1zm-13-7v1h-2v1h1v1h-3v-1h1v-1h-2v1h-1v-2h6zm-1 4v-1h1v3h-1v-1h-1v1h-1v-1h-1v1h-2v-1h1v-1h4zm-4-1v1h-1v-1h1zm19-2h-1v-1h1v1zm-13 4h1v1h-1v-1zm15 2h-1v-1h1v1zm-5 1v-1h1v1h-1zm-1-1h1v-3h2v-1h1v-1h-1v-1h-2v-1h-1v1h-1v-1h-1v1h-1v-1h-1v1h-1v1h-1v-1h1v-1h-4v1h2v1h-2v1h1v2h1v-1h2v2h1v-4h1v2h3v1h-2v1h2v1zm1-5h1v1h-1v-1zm-2 1h-1v-1h1v1z"/></svg>`,
     };
 
     return { icons };
