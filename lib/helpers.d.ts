5972374385dd5b7bacc5cb6c4662220dff6c96d5
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-07-11 13:49:07 +0200

    Update version to 0.1.99

--- lib/helpers.d.ts
+++ lib/helpers.d.ts
@@ -1,8 +1,10 @@
-import { Entity, Metadata, MetaKey } from './queries';
+import { Entity, MediaFile, Metadata, MetaKey } from './queries';
 declare type WeightedArrayConfig = {
     probability: number | "*";
 };
 export declare const reloadMediaAfterModalClose: (playerId: string) => void;
 export declare const randomizer: <T extends string>(input: Record<T, WeightedArrayConfig>) => T | undefined;
 export declare const getMetadataOfTypeFromEntity: (_entity: Entity, _key: MetaKey) => Metadata | undefined;
+export declare const getFirstValueOfPropertyFromEntity: (_entity: Entity, _property: `title` | `description`) => Metadata | undefined;
+export declare const getFirstMediafileWithFilelocationOfEntity: (_entity: Entity) => Promise<null | MediaFile>;
 export {};
