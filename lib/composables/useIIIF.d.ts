64e772ef99468e6cbd21fbd9b88de08ae2bd6225
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2022-08-23 08:13:35 +0200

    git remove -r --cached lib

--- lib/composables/useIIIF.d.ts
+++
@@ -1,7 +0,0 @@
-declare const useIIIF: (_url: string) => {
-    audioUrl: string;
-    noImageUrl: string;
-    generateUrl: (filename: string, format: "square" | "full", width?: string | "max", height?: number | undefined) => string;
-    generateInfoUrl: (filename: string) => string;
-};
-export { useIIIF };
--- lib/types.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type ImageSource = {
-    imageUrl: string;
-    infoJson: string;
-    fallBackUrl: string;
-    noImageUrl: string;
-    mediaType: object;
-};
-export declare type ModalState = "show" | "hide" | "loading" | "small";
