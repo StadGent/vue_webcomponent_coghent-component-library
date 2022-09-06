471506b762a42532031c0847903321c938e42bdf
Berend <berend@inuits.eu>
2022-09-06 16:51:24 +0200

    Add alert icon

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -59,7 +59,8 @@ type CustomIcon =
   | "file"
   | "send"
   | "undo"
-  | "downwardArrows";
+  | "downwardArrows"
+  | "alert";
 export default defineComponent({
   props: {
     icon: {
@@ -647,6 +648,7 @@ export default defineComponent({
 <path d="M2 10L22 2L18 22L2 10Z" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`,
       undo: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-rotate-ccw"><polyline points="1 4 1 10 7 10"></polyline><path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path></svg>`,
+      alert: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg>`,
     };
 
     return { icons };
